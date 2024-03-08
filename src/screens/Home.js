7b403ae32bc5383ba7b8cf2d461e74f02bfe2fc4
ElisaSPereira <pereiraelisa365@gmail.com>
2024-03-08 15:20:21 -0300

    Componente Post

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,21 +1,39 @@
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import Header from '../components/Header';
 import Stories from '../components/Stories';
+import Post from '../components/Post'
+import Perfil from '../assets/foto_mulher.jpg'
 
 
 export default function Home() {
+
   return (
-    <View 
-    style={{ 
-      marginVertical: 32,
-      marginHorizontal: 16, 
-      flex: 1 }}>
+    <ScrollView style={{ marginVertical: 32, marginHorizontal: 16 }}>
       <Header />
       <Stories />
+      <Post
+        profileImage={Perfil}
+        postImage={Perfil}
+        profileName="Fulana da Silva"
+        description="lorem lorem lorem, lorem lorem lorem, lorem lorem lorem, lorem lorem lorem"
+      />
 
-      
-    </View>
+      <Post
+        profileImage={Perfil}
+        postImage={Perfil}
+        profileName="Fulana da Silva"
+        description="lorem lorem lorem, lorem lorem lorem, lorem lorem lorem, lorem lorem lorem"
+      />
 
-    
+      <Post
+        profileImage={Perfil}
+        postImage={Perfil}
+        profileName="Fulana da Silva"
+        description="lorem lorem lorem, lorem lorem lorem, lorem lorem lorem, lorem lorem lorem"
+      />
+
+    </ScrollView>
+
+
   );
 }
